869a51a0e2863fa209e5279787c85af41626788e
Joel Bond <sys-maketecheasier@outlook.com>
2017-04-03 18:20:43 -0600

    Adjusted the code
    I added some explanations. The default name for the text file is Your
    Text File, and the default save location is your desktop. Please make
    sure to edit the save location, before running, otherwise it will not
    work!

--- Save2Txt.bat
+++ Save2Txt.bat
@@ -1,4 +1,7 @@
 @echo off
 Cls
+echo This will create a new text file, and then allow you to add text to it. By default, text starts at line 1, and continues down.
+echo Change the %userprofile% to your user folder, and it will then save the text file to your desktop.
+echo For more information, please check our Readme.md file.
 set /p t2a=Text to Add: 
-echo %t2a% >> C:\Users\joelt\Desktop\newtxt.txt
+echo %t2a% >> C:\Users\joelt\Desktop\Your Text File.txt
