4625f1e38c3ad8d69726dd4aea6636648bcfd5e7
Joel Bond <sys-maketecheasier@outlook.com>
2017-04-03 20:28:41 -0600

    Added color to some, tried creating new apps

--- Save2Txt.bat
+++ Save2Txt.bat
@@ -1,6 +1,8 @@
 @echo off
 Cls
-echoThis will create a new text file, and then allow you to add text to it. By default, text starts at line 1, and continues down.
+title Save Text to a Text Document
+color 1F
+echo This will create a new text file, and then allow you to add text to it. By default, text starts at line 1, and continues down.
 echo This command should default to your user folder, and it will then save the text file to your desktop.
 echo By default, the text file should be called Save2Txt Info. To add more info, run this command again and enter the info
 echo For more information, please check our Readme.md file.
