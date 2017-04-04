1735e48a7f54c4732368cd961a1cd2721165f72a
Joel Bond <sys-maketecheasier@outlook.com>
2017-04-03 20:33:06 -0600

    Adjusted and added color to Save2Txt

--- Save2Txt.bat
+++ Save2Txt.bat
@@ -1,7 +1,13 @@
 @echo off
+set NUM=0 1 2 3 4 5 6 7 8 9 A B C D E F
+for %%x in (%NUM%) do ( 
+    for %%y in (%NUM%) do (
+        color %%x%%y
+        timeout 1 >nul
+    )
+)
 Cls
 title Save Text to a Text Document
-color 1F
 echo This will create a new text file, and then allow you to add text to it. By default, text starts at line 1, and continues down.
 echo This command should default to your user folder, and it will then save the text file to your desktop.
 echo By default, the text file should be called Save2Txt Info. To add more info, run this command again and enter the info
