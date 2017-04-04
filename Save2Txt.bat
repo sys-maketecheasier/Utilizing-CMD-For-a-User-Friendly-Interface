06d7c3fd514e8de7edabc8cafab572b69b2c13a2
Joel Bond <sys-maketecheasier@outlook.com>
2017-04-03 20:50:16 -0600

    Reset all Commands

--- Save2Txt.bat
+++ Save2Txt.bat
@@ -2,13 +2,12 @@
 @echo off
 Cls
 title Save Text to a Text Document
-color 0F
 echo This will create a new text file, and then allow you to add text to it. By default, text starts at line 1, and continues down.
 echo This command should default to your user folder, and it will then save the text file to your desktop.
 echo By default, the text file should be called Save2Txt Info. To add more info, run this command again and enter the info
 echo For more information, please check our Readme.md file.
 set /p t2a=Text to Add: 
 echo %t2a% >> C:\Users\joelt\Desktop\Save2Txt.txt
-echo The command has completed. The following text: "%t2a%" has been added to the text file.
+echo The command has completed. The following text has been added to the text file: %t2a%
 Pause
 Goto Beginning
