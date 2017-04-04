5094dfff10fcf2767bfbe907f77cbff343790173
Joel Bond <sys-maketecheasier@outlook.com>
2017-04-03 20:13:21 -0600

    Updated Again

--- Save2Txt.bat
+++ Save2Txt.bat
@@ -1,6 +1,6 @@
 @echo off
 Cls
-echo This will create a new text file, and then allow you to add text to it. By default, text starts at line 1, and continues down.
+echoThis will create a new text file, and then allow you to add text to it. By default, text starts at line 1, and continues down.
 echo This command should default to your user folder, and it will then save the text file to your desktop.
 echo By default, the text file should be called Save2Txt Info. To add more info, run this command again and enter the info
 echo For more information, please check our Readme.md file.
