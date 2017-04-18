4e98e1bec9d0928cc17f54aede519f3cd2034952
sys-maketecheasier <sys.maketecheasier@outlook.com>
2017-04-18 09:01:48 -0600

    updated code

--- Save2Txt5.bat
+++ Save2Txt5.bat
@@ -5,7 +5,6 @@ title Save 2 Text
 echo Welcome to Save2Txt Version 5.6.7.0141
 echo Please follow the instructions below to add text to your text file.
 echo --------
-Pause
 echo Please choose where to save the text file (example: C:\Users\your username here\Desktop\)
 echo DO NOT INCLUDE THE FILE NAME! THAT COMES IN STEP 2
 set /p sl=Save Location: 
@@ -15,10 +14,9 @@ echo PLEASE REMEMBER TO PUT .TXT AFTER THE FILE NAME OTHERWISE FILL WILL BE CORR
 set /p fn=File Name:
 echo --------
 echo Now for the final step. Please choose the text you would like to add. To add more lines to the text file, please see
-the readme file for info on which one can add multiple lines of text to a text file.
+echo the readme file for info on which one can add multiple lines of text to a text file.
 set /p t2a=Text to Add: 
 echo Are you Ready?
 Pause
 echo %t2a% >> %sl%\%fn%
-Pause
 Goto BGG
