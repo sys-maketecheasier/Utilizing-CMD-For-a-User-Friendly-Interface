6b37f87adf6b0bfa70652533e23e2ca22940a984
Joel Bond <joeltbond@outlook.com>
2017-05-14 17:52:10 -0600

    added exit, save2txt, etc.

--- Advanced Dev. Tools/V1.bat
+++ Advanced Dev. Tools/V1.bat
@@ -1,12 +1,14 @@
 @ECHO OFF
 CLS
 :ORIGIN
+@echo OFF
+Cls
 ECHO 1. Save Text to a Text File
 ECHO 2. Adjust User Accounts and Admin Settings
 ECHO 3. Text To Speech
 ECHO 4. COMING SOON
 ECHO 5. COMING SOON
-ECHO 6. COMING SOON
+ECHO 6. Exit
 ECHO.
 
 CHOICE /C 123456 /M "Enter your choice: "
@@ -20,16 +22,25 @@ IF ERRORLEVEL 2 GOTO Operation2
 IF ERRORLEVEL 1 GOTO Operation1
 
 :Operation6
-echo This feature is coming soon. Press any key to go back to the main menu.
-PAUSE >NUL
-goto ORIGIN
-
+@echo all
+Cls
+echo Are you sure you would like to exit the program?
+echo Type Yes to exit, or type No to return to the main menu.
+set/p "cho=>"
+if %cho%==Yes goto LOCK
+if %cho%==yes goto LOCK
+if %cho%==no goto ORIGIN
+if %cho%==No goto ORIGIN
 :Operation5
+@echo all
+CLS
 echo This feature is coming soon. Press any key to go back to the main menu.
 PAUSE >NUL
 goto ORIGIN
 
 :Operation4
+@echo all
+CLS
 echo This feature is coming soon. Press any key to go back to the main menu.
 PAUSE >NUL
 goto ORIGIN
